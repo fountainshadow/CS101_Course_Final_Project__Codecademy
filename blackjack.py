711e4caedf9743e94bb86dccb2e985710e95a8d1
Lauren 87094 <lauren87094@gchq.gov.uk>
2023-02-13 11:02:14 +0000

    Re Defined the functions needed to allow the user to stick or twist

--- blackjack.py
+++ blackjack.py
@@ -3,52 +3,46 @@ import random
 from random import sample
 
 # Options from a deck of cards.
-cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-# Intro
-print("Welcome to Black Jack! You will be playing against the banker.")
-name = input("What is your name? ")
-print(f"Welcome to the game {name}!")
-
-# Starting Point
-user_cards = sample(cards, 2)
-print(f"Your starting cards are: {user_cards}")
-user_sum = 0
-for i in user_cards:
-    user_sum += i
-print(f"You current total is: {user_sum} out of 21")
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+user_cards = sample(deck, 2)
 
 # FUNCTIONS
-# Another Card
-def decision():
+# Sum of Cards
+def card_sum(user_cards):
+    sum = 0
+    for i in user_cards:
+        sum += i
+    print(f"Your current total is: {sum}")
+
+# Want Another Card - Question
+def stick_or_twist():
     decision = input("Would you like another card? Y/N: ")
     if decision == 'Y':
-        draw_card(decision)
+        twist()
     elif decision == 'N':
-        draw_card(decision)
-# Bust
-def bust(choice):
-    user_sum = 0
-    if user_sum > 21:
-        print("You have gone BUST! The banker has won...")
-    elif user_sum <= 21:
-        draw_card()
-
-# Stick or Twist
-def draw_card(choice):
-    if choice == 'Y':
-        user_cards.extend(sample(cards, 1))
-        print(f"You currently hold the cards: {user_cards}")
-        user_sum = 0
-        for i in user_cards:
-            user_sum += i
-        print(f"You current total is: {user_sum} out of 21")
-        decision()
-    elif choice == 'N':
-        print(f"You have stuck with the cards: {user_cards}")
+        stick()
     else:
-        print("Error.")
+        print("Error")
 
-choice = decision()
-print(choice)
-draw_card(choice)
+# Draw Again
+def twist():
+    user_cards.extend(sample(deck, 1))
+    print(f"You currently hold the cards: {user_cards}")
+    user_sum = card_sum(user_cards)
+
+# Stick With Cards
+def stick():
+    print(f"You are currently holding the cards: {user_cards}")
+    user_sum = card_sum(user_cards)
+
+# INTRO
+print("Welcome to Black Jack! You will be playing against the banker.")
+name = input("What is your name? ")
+print(f"WELCOME TO THE GAME {name}!")
+
+# STARTING POINT
+print(f"Your starting cards are: {user_cards}. You current total is: {sum(user_cards)} out of 21.")
+
+stick_or_twist()
+
+
